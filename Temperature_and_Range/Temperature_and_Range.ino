121abd46bbfdb8ddc5efefbbefad5bae1187bc37
sutekh137 <sutekh137@gmail.com>
2013-04-07 10:16:44 -0500

    DHT22 stuff
    Added dew point calculation to be ready when I implement the DHT22
    sensor.

--- Temperature_and_Range/Temperature_and_Range.ino
+++ Temperature_and_Range/Temperature_and_Range.ino
@@ -1,7 +1,8 @@
-#include <DHT22_430.h>
 
 // Includes
 #include <LiquidCrystal.h>
+#include <DHT22_430.h>
+#include <math.h>
 
 // Defines.
 // We will use empirically determined reference voltages, as even with a 3.3V
@@ -19,7 +20,7 @@
 #define MS_BETWEEN_READINGS  2000
 #define PIN_CHANGE_MODE  14
 #define MIN_MODE  1
-#define MAX_MODE  3
+#define MAX_MODE  4
 
 // Global variables
 LiquidCrystal Display(8, 9, 10, 11, 12, 13);
@@ -93,12 +94,12 @@ void DisplayTempOutputLine(byte pnDisplayLine, char* pcIntro) {
   ResetLCDLine(pnDisplayLine);
   Display.print(pcIntro);
   Display.print(gnTempF, 1);
-  if (gnTempF >= 0.00) {
+  if (gnTempF >= 0.00 && gnTempF < 100.00) {
     Display.print(DEGREE_SYMBOL);
   }
   Display.print("F ");
   Display.print(gnTempC, 1);
-  if (gnTempC >= 0.00) {
+  if (gnTempC >= 0.00 && gnTempC < 100.00) {
     Display.print(DEGREE_SYMBOL);
   }
   Display.print("C");
@@ -157,22 +158,50 @@ void BothInfo() {
 }
 
 void TempRH() {
-  boolean llSuccess = THSensor.get();
+  //boolean llSuccess = THSensor.get();
+  boolean llSuccess= true;
   if (llSuccess) {
     // Sensor reading was sucessful. We can check object properties then display.
-    float lnHumidity = THSensor.humidityX10()/10.0;
+    float lnHumidity = 80.0;
+    //float lnHumidity = THSensor.humidityX10()/10.0;
     // Display tempeature.
-    gnTempC = THSensor.temperatureX10()/10.0;
+    gnTempC = 30.0;
+    //gnTempC = THSensor.temperatureX10()/10.0;
     gnTempF = (gnTempC * 9/5) + 32;
     DisplayTempOutputLine(0, "T: ");
     // Display Relative Humidity.
     ResetLCDLine(1);
-    Display.print("RH: ");
-    Display.print(lnHumidity, 1);
-    Display.print(" %");
+    Display.print("H: ");
+    Display.print(lnHumidity, 0);
+    Display.print("%");
+    // Calculate dew point and display it as well.
+    float lnDewPointC = CalcDP(gnTempC, lnHumidity);
+    float lnDewPointF = (lnDewPointC * 9/5) + 32;
+    Display.print(" DP: ");
+    Display.print(lnDewPointF, 0);
+    Display.print(DEGREE_SYMBOL);    
+    Display.print("F");    
   } 
   else {
+    ResetLCDLine(1);
+    ResetLCDLine(0);
+    Display.print("(No T/RH info.)");
   }
+}
+
+float CalcDP(float pnT, float pnRH)
+{
+  // Approximate dewpoint using the formula from Wikipedia's article on dewpoint
+
+  float lnDP = 0.0;
+  float gTRH = 0.0;
+  float a = 17.271;
+  float b = 237.7;
+
+  gTRH = ((a * pnT)/(b + pnT)) + logf(pnRH/100);
+  lnDP = (b * gTRH)/(a - gTRH);
+
+  return lnDP;
 }
 
 void loop() {
